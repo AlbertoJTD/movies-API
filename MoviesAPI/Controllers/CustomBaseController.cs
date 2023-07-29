a4ab6c8a0e32520ef885f684975894b767f63fc7
AlbertoJTD <albertojtd@outlook.com>
2023-07-28 20:01:52 -0600

    Add ReviewController

--- MoviesAPI/Controllers/CustomBaseController.cs
+++ MoviesAPI/Controllers/CustomBaseController.cs
@@ -31,6 +31,11 @@ namespace MoviesAPI.Controllers
 		protected async Task<List<TDTO>> Get<TEntidad, TDTO>(PaginacionDTO paginacionDTO) where TEntidad : class
 		{
 			var queryable = context.Set<TEntidad>().AsQueryable();
+			return await Get<TEntidad, TDTO>(paginacionDTO, queryable);
+		}
+
+		protected async Task<List<TDTO>> Get<TEntidad, TDTO>(PaginacionDTO paginacionDTO, IQueryable<TEntidad> queryable) where TEntidad : class
+		{
 			await HttpContext.InsertarParametrosPaginacion(queryable, paginacionDTO.CantidadRegistrosPorPagina);
 
 			var entidades = await queryable.Paginar(paginacionDTO).ToListAsync();
