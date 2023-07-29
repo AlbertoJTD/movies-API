65d4ad6a41a0dd989c064917c5e7eb22b745c07f
AlbertoJTD <albertojtd@outlook.com>
2023-07-29 12:22:11 -0600

    Add test for Get method in GenerosController

--- MoviesAPI.Tests/PruebasUnitarias/GenerosControllerTests.cs
+++ MoviesAPI.Tests/PruebasUnitarias/GenerosControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoviesAPI.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,28 @@ namespace MoviesAPI.Tests.PruebasUnitarias
 	[TestClass]
 	public class GenerosControllerTests: BasePruebas
 	{
+		[TestMethod]
+		public async Task ObtenerTodosLosGeneros()
+		{
+			// Preparacion
+			var nombreBD = Guid.NewGuid().ToString();
+			var contexto = ConstruirContext(nombreBD);
+			var mapper = ConfigurarAutoMapper();
 
+			contexto.Generos.Add(new Entidades.Genero() { Nombre = "Genero 1" });
+			contexto.Generos.Add(new Entidades.Genero() { Nombre = "Genero 2" });
+			await contexto.SaveChangesAsync();
+
+			var contexto2 = ConstruirContext(nombreBD);
+
+			// Prueba
+			var controller = new GenerosController(contexto2, mapper);
+			var respuesta = await controller.Get();
+
+			// Verificacion
+			var generos = respuesta.Value;
+
+			Assert.AreEqual(2, generos.Count);
+		}
 	}
 }
