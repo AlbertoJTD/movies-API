12d2fac744cb89cb6df8939eacf9ee5fa9f342a9
AlbertoJTD <albertojtd@outlook.com>
2023-07-30 16:11:00 -0600

    Add test to get the nearest theaters in SalasDeCineController

--- MoviesAPI.Tests/PruebasUnitarias/SalsasDeCineControllerTest.cs
+++ MoviesAPI.Tests/PruebasUnitarias/SalsasDeCineControllerTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MoviesAPI.Controllers;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entidades;
+using NetTopologySuite.Geometries;
+using NetTopologySuite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,42 @@ using System.Threading.Tasks;
 
 namespace MoviesAPI.Tests.PruebasUnitarias
 {
-	public class SalsasDeCineControllerTest
+	[TestClass]
+	public class SalsasDeCineControllerTest: BasePruebas
 	{
+		[TestMethod]
+		public async Task ObtenerSalasDeCineMenorOIgualA5KM()
+		{
+			// Preparacion
+			var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+			using (var context = LocalDbDatabaseInitializer.GetDbContextLocalDb(false))
+			{
+				var salasDeCine = new List<SalaDeCine>()
+				{
+					new SalaDeCine{ Nombre = "Sala 1", Ubicacion = geometryFactory.CreatePoint(new Coordinate(-69.9388777, 18.4839233)) }
+				};
+
+				context.AddRange(salasDeCine);
+				await context.SaveChangesAsync();
+			}
+
+			// Prueba
+			var filtro = new SalaDeCineCercanoFiltroDTO()
+			{
+				DistanciaEnKms = 5,
+				Latitud = 18.481139,
+				Longitud = -69.938950
+			};
+
+			// Verificacion
+			using (var context = LocalDbDatabaseInitializer.GetDbContextLocalDb(false))
+			{
+				var mapper = ConfigurarAutoMapper();
+				var controller = new SalasDeCineController(context, mapper, geometryFactory);
+				var respuesta = await controller.Cercanos(filtro);
+				var valor = respuesta.Value;
+				Assert.AreEqual(1, valor.Count);
+			}
+		}
 	}
 }
