5ed6e543d39df5a4d79e89a08e9df0eedd646f47
AlbertoJTD <albertojtd@outlook.com>
2023-07-30 16:16:12 -0600

    Add CrearPeliculas method

---
+++ MoviesAPI.Tests/PruebasUnitarias/ReviewControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Tests.PruebasUnitarias
+{
+	[TestClass]
+	public class ReviewControllerTests: BasePruebas
+	{
+		private void CrearPeliculas(string nombreDB)
+		{
+			var contexto = ConstruirContext(nombreDB);
+			contexto.Peliculas.Add(new Pelicula() { Titulo = "pelicula 1" });
+			contexto.SaveChanges();
+		}
+	}
+}
