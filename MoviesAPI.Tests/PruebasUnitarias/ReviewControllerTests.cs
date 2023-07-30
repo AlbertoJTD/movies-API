85936f5fd7ac2d9278e6388197fa9e85bc1209e9
AlbertoJTD <albertojtd@outlook.com>
2023-07-30 16:27:29 -0600

    Add test when a user wants to create a second review in ReviewController

--- MoviesAPI.Tests/PruebasUnitarias/ReviewControllerTests.cs
+++ MoviesAPI.Tests/PruebasUnitarias/ReviewControllerTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoviesAPI.Controllers;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +20,40 @@ namespace MoviesAPI.Tests.PruebasUnitarias
 			var contexto = ConstruirContext(nombreDB);
 			contexto.Peliculas.Add(new Pelicula() { Titulo = "pelicula 1" });
 			contexto.SaveChanges();
+		}
+
+		[TestMethod]
+		public async Task UsuarioNoPuedeCrearDosReviewsParaLaMismaPelicula()
+		{
+			// Preparacion
+			var nombreBD = Guid.NewGuid().ToString();
+			var contexto = ConstruirContext(nombreBD);
+			CrearPeliculas(nombreBD);
+
+			var peliculaId = contexto.Peliculas.Select(x => x.Id).First();
+			var review1 = new Review()
+			{
+				PeliculaId = peliculaId,
+				UsuarioId = usuarioPorDefectoId,
+				Puntuacion = 5
+			};
+
+			contexto.Add(review1);
+			await contexto.SaveChangesAsync();
+
+			// Prueba
+			var contexto2 = ConstruirContext(nombreBD);
+			var mapper = ConfigurarAutoMapper();
+
+			var controller = new ReviewController(contexto2, mapper);
+			controller.ControllerContext = ConstruirControllerContext();
+
+			var reviewCreacionDTO = new ReviewCreacionDTO { Puntuacion = 5 };
+			var respuesta = await controller.Post(peliculaId, reviewCreacionDTO);
+
+			// Verificacion
+			var valor = respuesta as IStatusCodeActionResult;
+			Assert.AreEqual(400, valor.StatusCode.Value);
 		}
 	}
 }
