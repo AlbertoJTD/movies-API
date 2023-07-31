8305411ecb9e726aded9bab26b2a427b1c202626
AlbertoJTD <albertojtd@outlook.com>
2023-07-30 18:05:17 -0600

    add integration tests for ReviewsController

---
+++ MoviesAPI.Tests/PruebasDeIntegracion/ReviewControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using MoviesAPI.DTOs;
+using MoviesAPI.Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesAPI.Tests.PruebasDeIntegracion
+{
+	[TestClass]
+	public class ReviewControllerTests: BasePruebas
+	{
+		private static readonly string url = "/api/peliculas/1/reviews";
+
+		[TestMethod]
+		public async Task ObtenerReviews_Devuelve404PeliculaInexistente()
+		{
+			var nombreBD = Guid.NewGuid().ToString();
+			var factory = ConstruirWebApplicationFactory(nombreBD);
+
+			var cliente = factory.CreateClient();
+			var respuesta = await cliente.GetAsync(url);
+			Assert.AreEqual(404, (int)respuesta.StatusCode);
+		}
+
+		[TestMethod]
+		public async Task ObtenerReviews_DevuelveListadoVacio()
+		{
+			var nombreBD = Guid.NewGuid().ToString();
+			var factory = ConstruirWebApplicationFactory(nombreBD);
+			var context = ConstruirContext(nombreBD);
+			context.Peliculas.Add(new Pelicula() { Titulo = "Película 1" });
+			await context.SaveChangesAsync();
+
+			var cliente = factory.CreateClient();
+			var respuesta = await cliente.GetAsync(url);
+
+			respuesta.EnsureSuccessStatusCode();
+
+			var reviews = JsonConvert.DeserializeObject<List<ReviewDTO>>(await respuesta.Content.ReadAsStringAsync());
+			Assert.AreEqual(0, reviews.Count);
+		}
+	}
+}
